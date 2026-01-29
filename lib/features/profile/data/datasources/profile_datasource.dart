1f56c62f1f2ae085411614f67b610d470ec88381
Bibek <npbibek94@gmail.com>
2026-01-29 10:47:15 +0545

    feat(profile): update profile data source interface for multi-platform uploads

---
+++ lib/features/profile/data/datasources/profile_datasource.dart
@@ -0,0 +1,23 @@
+import 'package:image_picker/image_picker.dart';
+import 'package:play_sync_new/features/profile/data/models/profile_response_model.dart';
+
+/// Abstract interface for profile data source
+abstract interface class IProfileDataSource {
+  /// Get current user profile
+  Future<ProfileResponseModel> getProfile();
+
+  /// Update user profile
+  Future<ProfileResponseModel> updateProfile(Map<String, dynamic> profileData);
+
+  /// Upload profile picture
+  Future<String> uploadProfilePicture(XFile image);
+
+  /// Upload cover picture
+  Future<String> uploadCoverPicture(XFile image);
+
+  /// Upload gallery pictures
+  Future<List<String>> uploadGalleryPictures(List<XFile> images);
+
+  /// Delete profile picture
+  Future<bool> deleteProfilePicture();
+}
