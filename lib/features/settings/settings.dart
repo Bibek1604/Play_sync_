220f191e2317d0d59dacba60b4fa62db80eec8ba
Bibek <npbibek94@gmail.com>
2026-02-20 00:51:48 +0545

    chore(settings): update barrel export to include all new settings files

--- lib/features/settings/settings.dart
+++ lib/features/settings/settings.dart
@@ -18,3 +18,12 @@
 library;
 
 export 'presentation/presentation.dart';
+export 'domain/entities/app_settings.dart';
+export 'data/settings_repository.dart';
+export 'presentation/providers/theme_provider.dart';
+export 'presentation/providers/language_provider.dart';
+export 'presentation/providers/notification_prefs_provider.dart';
+export 'presentation/pages/app_settings_page.dart';
+export 'presentation/pages/about_page.dart';
+export 'presentation/widgets/theme_preview_card.dart';
+export 'presentation/widgets/accent_color_picker.dart';
